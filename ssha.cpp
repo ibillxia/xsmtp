448405fbe25d7f512779bd4272478d5fe8731236
Rob Chiarelli <rob.chiarelli@nyu.edu>
2016-04-28 16:40:51 -0400

    added ssha.cpp

---
+++ ssha.cpp
@@ -0,0 +1,77 @@
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+const int salt_len = 16;
+int pass_len = 0;
+const string lookup = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+char* create_salt(char* salt) {
+	// create randomized string of characters of length salt_len
+	srand(time(NULL));
+	for (int i = 0; i < salt_len; i++) {
+		salt[i] = lookup[rand() % (lookup.size() - 1)];
+	}
+}
+
+char* add_salt(char* pass, char* salt, char* salted_pass) {
+		
+	// combine original password and salt
+	// strcat was causing issues so i wrote it myself
+	for (int i = 0; i < pass_len; i++) { 
+		salted_pass[i] = pass[i]; 
+	}
+	for (int i = pass_len; i < pass_len + salt_len; i++) { 
+		salted_pass[i] = salt[i - pass_len]; 
+	}
+}
+
+void create_hash(unsigned char* pass_plain, unsigned char* pass_enc, int len) {
+	SHA256_CTX context;
+	
+	SHA256_Init(&context);
+	SHA256_Update(&context, pass_plain, len);
+	SHA256_Final(pass_enc, &context);
+}
+
+void ssha(string user, string pass_str) {
+	
+	// set password length global variable
+	pass_len = pass_str.size();
+	
+	// create and fill array to store password
+	char pass[pass_str.size()];
+	for (int i = 0; i < pass_str.size(); i++) { pass[i] = pass_str[i]; }
+	
+	// create and fill array to store salt
+	char salt[salt_len];
+	create_salt(salt);
+	
+	// create and fill array to store password + salt
+	char salted_pass[pass_len + salt_len];
+	add_salt(pass, salt, salted_pass);
+	
+	// copy signed password array into unsigned array for use by sha256
+	unsigned char pass_plain[pass_len + salt_len];
+	for (int i = 0; i < pass_len + salt_len; i++) { pass_plain[i] = salted_pass[i]; }
+	
+	// create and fill array to store sha256 output
+	unsigned char pass_enc[SHA256_DIGEST_LENGTH];
+	create_hash(pass_plain, pass_enc, sizeof(salted_pass));
+	
+	// write username , hashed password, and salt to text file
+	
+}
+
+int main() {
+	string user = "robobert"
+	string pass_str = "password";
+	
+	ssha(user, pass_str);
+	
+	return 0;
+}
